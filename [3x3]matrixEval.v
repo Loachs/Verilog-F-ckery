e2cf25d7b97f4adb7ce395f404a79608c04c39db
Kyle Haltmeyer <KyleHaltmeyer@gmail.com>
2026-09-09 18:42:35 -0400

    Updated conditional blocks to work so decrease mem usage
    added end breaks in near the if lines to reduce the time to pass through + fixed the code to work

--- [3x3]matrixEval.v
+++ [3x3]matrixEval.v
@@ -32,79 +32,104 @@ always @* begin
 	nothing =	8b'0;
 	
 	//Diag
-	
-	if (xval[0] & xval[4] & xval[8] & !(xval[6] | xval[2]))
-		diag = //high val
-	break
+	begin : diag_Val
+		if (xval[0] & xval[4] & xval[8] & !(xval[6] | xval[2])) begin : 
+			diag = //high val
+			disable diag_Val;
+		end
+			
+		if (xval[2] & xval[4] & xval[6] & !(xval[0] | xval[8])) begin :
+			diag = // High Val
+			disable diag_Val;
+		end
 		
-	if (xval[2] & xval[4] & xval[6] & !(xval[0] | xval[8]))
-		diag = // High Val
-	break
-	
-	if (((xval[1] & xval[3]) | (xval[5] & xval[7]) | ((xval[2] | xval[6]) & xval[4])) & (!(xval[0] | xval[9])))
-		diag = // mid val
-	break
-	
-	if (((xval[1] & xval[5]) | (xval[3] & xval[7]) | ((xval[0] | xval[8]) & xval[4])) & (!(xval[2] | xval[6])))
-		diag = // mid val	
-	break
+		if (((xval[1] & xval[3]) | (xval[5] & xval[7]) | ((xval[2] | xval[6]) & xval[4])) & (!(xval[0] | xval[9]))) begin :
+			diag = // mid val
+			disable diag_Val;
+		end
+		
+		if (((xval[1] & xval[5]) | (xval[3] & xval[7]) | ((xval[0] | xval[8]) & xval[4])) & (!(xval[2] | xval[6]))) begin :
+			diag = // mid val
+			disable diag_Val;	
+		end
+	end
 	
 	// curve
-	if (!xval[0] & xval[1] & xval[2] & xval[3] & xval[6] & !(xval[5] | xval[7] |xval[8]))
-		curve = // high val
-	break
-	
-	if (!xval[2] & xval[0] & xval[1] & xval[5] & xval[8] & !(xval[3] | xval[6] |xval[7]))
-		curve = // high val
-	break
-	
-	if (!xval[8] & xval[6] & xval[7] & xval[2] & xval[5] & !(xval[0] | xval[1] |xval[3]))
-		curve = // high val
-	break
-	
-	if (!xval[6] & xval[0] & xval[3] & xval[7] & xval[8] & !(xval[1] | xval[2] |xval[5]))
-		curve = // high val
-	break
-	
-	if ((row[1] | row[3]) & (col[1] | col[3]))
-		curve = // high val
-	break
-	
-	if (((row[1] | row[3]) & (xval[3] | xval[5])) | ((col[1] | col[3]) & (xval[1] | xval[7])))
-		curve = //mid val
-	break
-	
-	if ((row[2] & (row[1] | row[3]) | (col[2] & (col[1] | col[2]))
-		curve = // Nothing
-	break
-	
-	if (row[1] | row[2] | row[3] | col[1] | col[2] | col[3])
-		curve = // nothing
-	break
-	
-	if (((xval[0] | xval[6]) & xval[3]) | ((xval[1] | xval[7]) & xval[4]) | ((xval[2] | xval[8]) & xval[5])
-		curve = //nothing
-	break
-	
-	if (((xval[0] | xval[2]) & xval[1]) | ((xval[3] | xval[5]) & xval[4]) | ((xval[6] | xval[8]) & xval[7])
-		curve = // nothing
-	break
+	begin : curve_Val
+		if (!xval[0] & xval[1] & xval[2] & xval[3] & xval[6] & !(xval[5] | xval[7] |xval[8])) begin :
+			curve = // high val
+			disable curve_Val;
+		end
+		
+		if (!xval[2] & xval[0] & xval[1] & xval[5] & xval[8] & !(xval[3] | xval[6] |xval[7])) begin :
+			curve = // high val
+			disable curve_Val;
+		end
+		
+		if (!xval[8] & xval[6] & xval[7] & xval[2] & xval[5] & !(xval[0] | xval[1] |xval[3])) begin :
+			curve = // high val
+			disable curve_Val;
+		end
+		
+		if (!xval[6] & xval[0] & xval[3] & xval[7] & xval[8] & !(xval[1] | xval[2] |xval[5])) begin :
+			curve = // high val
+			disable curve_Val;
+		end
+		
+		if ((row[1] | row[3]) & (col[1] | col[3])) begin :
+			curve = // high val
+			disable curve_Val;
+		end
+		
+		if (((row[1] | row[3]) & (xval[3] | xval[5])) | ((col[1] | col[3]) & (xval[1] | xval[7]))) begin :
+			curve = //mid val
+		end
+		
+		if ((row[2] & (row[1] | row[3]) | (col[2] & (col[1] | col[2])) begin :
+			curve = // Nothing
+			disable curve_Val;
+		end
+		
+		if (row[1] | row[2] | row[3] | col[1] | col[2] | col[3]) begin :
+			curve = // nothing
+			disable curve_Val;
+		end
+		
+		if (((xval[0] | xval[6]) & xval[3]) | ((xval[1] | xval[7]) & xval[4]) | ((xval[2] | xval[8]) & xval[5]) begin :
+			curve = //nothing
+			disable curve_Val;
+		end
+		
+		if (((xval[0] | xval[2]) & xval[1]) | ((xval[3] | xval[5]) & xval[4]) | ((xval[6] | xval[8]) & xval[7]) begin :
+			curve = // nothing
+			disable curve_Val;
+		end
+	end
 	
 	// nothing
-	
-	if (bits <= 1)
-		nothing = // High
-	break
-	
-	if (bits = 2)
-		nothing = //mid
-	break
+	begin : nothing_val
+		if (bits <= 1) begin : 
+			nothing = // High
+			disable nothing_val;
+		end
+		
+		if (bits = 2) begin :
+			nothing = //mid
+			disable nothing_val;
+		end
 
-	if (bits = 3)
-		nothing = //low
-	break
-	
-	else 
+		if (bits = 3) begin :
+			nothing = //low
+			disable nothing_val;
+		end
+		
+		else begin :
+			nothing = //zero
+			disable nothing_val;
+		end
+	end
+end
+endmodule
 		nothing = //zero
 	break
 	

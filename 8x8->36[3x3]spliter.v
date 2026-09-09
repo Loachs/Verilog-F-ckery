711f4fb486a1ce2d22186d696b5c65a487f43412
Kyle Haltmeyer <KyleHaltmeyer@gmail.com>
2026-09-08 22:55:23 -0400

    Removed all y values since we can just use x
    Refactor output assignments for clarity and remove unused yval array.

--- 8x8->36[3x3]spliter.v
+++ 8x8->36[3x3]spliter.v
@@ -1,5 +1,13 @@
 module retardmaxedbullshit(
 
+/*
+{--------------------------------}
+You must have over 176 pins to
+allocate to use this because
+of this code -READ THIS-
+{--------------------------------}
+*/
+
 input [7:0] row1,
 input [7:0] row2,
 input [7:0] row3,
@@ -8,15 +16,17 @@ input [7:0] row5,
 input [7:0] row6,
 input [7:0] row7,
 input [7:0] row8,
+
 input clk,
+
 input [2:0] muxcnt,
 
-output reg [8:0] r1c1x, output reg [8:0] r1c1y, 
-output reg [8:0] r2c1x, output reg [8:0] r2c1y,
-output reg [8:0] r3c1x, output reg [8:0] r3c1y,
-output reg [8:0] r4c1x, output reg [8:0] r4c1y,
-output reg [8:0] r5c1x, output reg [8:0] r5c1y,
-output reg [8:0] r6c1x, output reg [8:0] r6c1y
+output reg [8:0] r1c1x,
+output reg [8:0] r2c1x,
+output reg [8:0] r3c1x,
+output reg [8:0] r4c1x,
+output reg [8:0] r5c1x,
+output reg [8:0] r6c1x
 ); 
 
 wire [7:0] rows [0:7];
@@ -30,7 +40,6 @@ assign rows[6] = row7;
 assign rows[7] = row8;
 
 wire [8:0] xval [0:5][0:5];
-wire [8:0] yval [0:5][0:5];
 
 genvar r, c;
 generate 
@@ -45,29 +54,27 @@ generate
 			assign bot = rows[r+2] [7-c -: 3];
 			
 			assign xval[r][c] = {top, mid, bot};
-			assign yval[r][c] = {top[0], mid[0], bot[0],
-										top[1], mid[1], bot[1],
-										top[2], mid[2], bot[2]};
 		end
 	end
 endgenerate
 
 always @* begin
 	if (muxcnt <= 3'b101) begin
-		r1c1x = xval[0][muxcnt]; r1c1y = yval[0][muxcnt];
-		r2c1x = xval[1][muxcnt]; r2c1y = yval[1][muxcnt];
-		r3c1x = xval[2][muxcnt]; r3c1y = yval[2][muxcnt];
-		r4c1x = xval[3][muxcnt]; r4c1y = yval[3][muxcnt];
-		r5c1x = xval[4][muxcnt]; r5c1y = yval[4][muxcnt];
-		r6c1x = xval[5][muxcnt]; r6c1y = yval[5][muxcnt];
+		r1c1x = xval[0][muxcnt];
+		r2c1x = xval[1][muxcnt];
+		r3c1x = xval[2][muxcnt];
+		r4c1x = xval[3][muxcnt];
+		r5c1x = xval[4][muxcnt];
+		r6c1x = xval[5][muxcnt];
 	end
 	else begin
-		r1c1x = 9'b0; r1c1y = 9'b0;
-		r2c1x = 9'b0; r2c1y = 9'b0;
-		r3c1x = 9'b0; r3c1y = 9'b0;
-		r4c1x = 9'b0; r4c1y = 9'b0;
-		r5c1x = 9'b0; r5c1y = 9'b0;
-		r6c1x = 9'b0; r6c1y = 9'b0;
+		r1c1x = 9'b0;
+		r2c1x = 9'b0;
+		r3c1x = 9'b0;
+		r4c1x = 9'b0;
+		r5c1x = 9'b0
+		r6c1x = 9'b0;
 	end
 end
+
 endmodule
